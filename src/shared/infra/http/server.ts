c885cf8be1717e3a6de591c692b525271da9514e
Guilherme Garcia <guillherme966@gmail.com>
2021-04-15 19:56:08 -0300

    Seed

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Request, Response} from 'express';
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "@shared/infra/typeorm";
+import createConnection from "@shared/infra/typeorm";
 
 import "@shared/container";
 
@@ -11,6 +11,7 @@ import { AppError } from "@shared/errors/AppError";
 import { router } from './routes';
 import swaggerFile from "../../../swagger.json";
 
+createConnection();
 const app = express();
 
 app.use(express.json());
