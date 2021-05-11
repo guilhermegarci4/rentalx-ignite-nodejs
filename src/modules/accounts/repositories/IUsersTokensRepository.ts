7b0a60547665a32b7100304aefde3875875680b9
Guilherme Garcia <guillherme966@gmail.com>
2021-05-11 19:28:12 -0300

    Auth

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -9,6 +9,10 @@ interface IUsersTokensRepository {
         refresh_token,
         user_id
     }: ICreateUserTokenDTO): Promise<UsersTokens>;
+
+    findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UsersTokens>
+
+    deleteById(id: string): Promise<void>
 }
 
 export { IUsersTokensRepository }
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,13 +2,21 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import { AppError } from "@shared/errors/AppError";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokenRepository";
+import auth from "@config/auth";
 
 interface IPayload {
     sub: string;
 }
 
-export async function ensureAuthenticate(request: Request, response: Response, next: NextFunction ) {
+export async function ensureAuthenticate(
+    request: Request, 
+    response: Response, 
+    next: NextFunction 
+) {
     const authHeader = request.headers.authorization;
+
+    const userTokenRepository = new UsersTokensRepository();
 
     if(!authHeader) {
         throw new AppError("Token missing", 401);
@@ -17,10 +25,15 @@ export async function ensureAuthenticate(request: Request, response: Response, n
     const [, token] = authHeader.split(" ");
 
     try {
-        const { sub: user_id } = verify(token, "d3e046e96c35632a66db27bbf3021856") as IPayload;
+        const { sub: user_id } = verify(
+            token,
+            auth.secret_refresh_token
+        ) as IPayload;
         
-        const usersRepository = new UsersRepository();
-        const user = await usersRepository.findById(user_id);
+        const user = await userTokenRepository.findByUserIdAndRefreshToken(
+            user_id,
+            token
+        );
 
         if(!user) {
             throw new AppError("User does not exists!", 401);
