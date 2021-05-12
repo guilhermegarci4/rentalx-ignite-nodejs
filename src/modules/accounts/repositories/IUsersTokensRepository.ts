0cc81dd02c7cba87b86f6da094ba03bc2a211628
Guilherme Garcia <guillherme966@gmail.com>
2021-05-11 21:11:17 -0300

    Reset Password

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -13,6 +13,8 @@ interface IUsersTokensRepository {
     findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UsersTokens>
 
     deleteById(id: string): Promise<void>
+
+    findByRefreshToken(refresh_token: string): Promise<UsersTokens>;
 }
 
 export { IUsersTokensRepository }
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,9 +2,16 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateprovider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
+import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 
 
 container.registerSingleton<IDateProvider> (
     "DateProvider",
     DayjsDateProvider
+);
+
+container.registerInstance<IMailProvider> (
+    "EtherealMailProvider",
+    new EtherealMailProvider()
 );
