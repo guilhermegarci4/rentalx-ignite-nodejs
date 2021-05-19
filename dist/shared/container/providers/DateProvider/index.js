7c353c492bf38c70abb57335d56c88af25e4070e
Guilherme Garcia <guillherme966@gmail.com>
2021-05-19 20:25:03 -0300

    .gitignore is now working

--- dist/shared/container/providers/DateProvider/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _DayjsDateprovider = require("./implementations/DayjsDateprovider");
-
-_tsyringe.container.registerSingleton("DateProvider", _DayjsDateprovider.DayjsDateProvider);
--- dist/src/shared/container/providers/DateProvider/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tsyringe_1 = require("tsyringe");
-var DayjsDateprovider_1 = require("./implementations/DayjsDateprovider");
-tsyringe_1.container.registerSingleton("DateProvider", DayjsDateprovider_1.DayjsDateProvider);
